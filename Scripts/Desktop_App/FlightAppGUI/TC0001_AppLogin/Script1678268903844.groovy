6fb03460f5c7c7811a9b765daec230ca89b00929
VersionControlWS <versioncontrolws@gmail.com>
2023-03-08 15:55:31 +0530

    FlightGUI winappdriver

---
+++ Scripts/Desktop_App/FlightAppGUI/TC0001_AppLogin/Script1678268903844.groovy
@@ -0,0 +1,39 @@
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+'https://docs-dev.katalon.com/docs/author/keywords/using-keywords-in-katalon-studio/windows-testing/execute-windows-commands-in-katalon-studio'
+//String cmd = "start C:\\Users\\Lenovo UTH-UK\\Desktop\\driver.bat"
+//Runtime.getRuntime().exec(cmd)
+
+//Runtime.runtime.exec("start C:\\Users\\Lenovo UTH-UK\\Desktop\\driver.bat")
+
+'https://docs.katalon.com/docs/create-tests/keywords/using-keywords-in-katalon-studio/windows-testing/execute-windows-commands-in-katalon-studio'
+import com.kms.katalon.core.configuration.RunConfiguration
+/**
+ * Execute a batch file situated in the KS project directory.
+ * @param batchFile (String) e.g. "myfile.bat"
+ */
+
+  String bf = "C:/Users/Lenovo UTH-UK/Desktop" + '/' + "driver.bat"
+  //comment("Running batch file: " + bf)
+  Runtime.runtime.exec(bf)
+
+//Windows.startApplication("C:\\Program Files (x86)\\Micro Focus\\UFT One\\samples\\Flights Application\\FlightsGUI.exe")
